c35f09282312038ef11876bb0241e87e6d191ee9
miftah.zulfikar <miftah.zulfikar@tokopedia.com>
2020-07-15 11:15:49 +0700

    remove my local story and package testing

---
+++ pages/stories/desktop/test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "unify-react-desktop/build/Button";
+
+export default function () {
+  return (
+    <div>
+      <h1>Desktop test story</h1>
+      <Button filled main>
+        Button
+      </Button>
+    </div>
+  );
+}
---
+++ pages/stories/mobile/test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "unify-react-mobile/build/Button";
+
+export default function () {
+  return (
+    <div>
+      <h1>mobile test story</h1>
+      <Button filled main>
+        Button
+      </Button>
+    </div>
+  );
+}
