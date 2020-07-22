2bbf1bebc386503871b420a17077f30b1ae37639
miftah.zulfikar <miftah.zulfikar@tokopedia.com>
2020-07-22 11:22:50 +0700

    test tooltip-v2 desktop

--- pages/stories/desktop/test.js
+++ pages/stories/desktop/test.js
@@ -1,13 +1,40 @@
-import React from "react";
-import Button from "unify-react-desktop/build/Button";
+import React, { useRef } from "react";
+import TooltipV2 from "unify-react-desktop/build/TooltipV2";
+import { css } from "react-emotion";
+
+const _center = css`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+`;
+
+const _link = css`
+  cursor: pointer;
+  outline: none;
+  padding: 8px;
+  margin: 8px;
+  border: solid 1px #e0e0e0;
+  border-radius: 8px;
+  background: #fafafa;
+`;
 
 export default function () {
+  const targetRef = useRef();
+  const props = {
+    placement: "bottom",
+    trigger: "hover",
+    onMouseEnter: console.log("onMouseEnter"),
+    onMouseLeave: console.log("onMouseLeave"),
+    onClick: console.log("onClick"),
+  };
   return (
-    <div>
-      <h1>Desktop test story</h1>
-      <Button filled main>
-        Button
-      </Button>
+    <div className={_center}>
+      <button className={_link} ref={targetRef}>
+        Hover Me
+      </button>
+      <TooltipV2 targetRef={targetRef} {...props}>
+        Make Tooltip as brief as possible. no 2 lines
+      </TooltipV2>
     </div>
   );
 }
